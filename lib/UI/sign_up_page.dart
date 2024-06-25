eb31b66f43f8720ade61f16d891366db48a445c9
MaxJakaria <maxjakaria825@gmail.com>
2024-06-25 15:12:24 +0600

    Online/ofline feature

--- lib/UI/sign_up_page.dart
+++ lib/UI/sign_up_page.dart
@@ -79,6 +79,8 @@ class _SignUpPageState extends State<SignUpPage> {
                             'name': name,
                             'email': email,
                             'about': "اَلْحَمْدُ لِلَّٰهِ",
+                            'is_online': '',
+                            'last_active': ''
                           }).then((_) {
                             Navigator.pop(context);
 
